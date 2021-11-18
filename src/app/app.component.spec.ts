d1686d9878b03eee4fcb216cdb700e676f215f99
Evgeniy Titov <evgeniy.titov@gmail.com>
2021-11-18 20:41:06 +0300

    commit 18/11/2021

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Registration'`, () => {
+  it(`should have as title 'LoginPage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Registration');
+    expect(app.title).toEqual('LoginPage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Registration app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LoginPage app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Registration';
+export class AppComponent implements  OnInit{
+
+  title = 'Регистрация';
+  regForm!: FormGroup;
+  submitted: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+  }
+
+  ngOnInit() {
+    this.regForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      passConfirm: ["", [Validators.required]],
+      rules: ["", [Validators.required]],
+      privacy: ["", [Validators.required]]
+    }, {validators: this.pass()})
+  }
+
+  get f() { return this.regForm.controls; }
+
+  submit () {
+    this.submitted = true;
+
+    if(this.regForm.invalid) {
+      return console.log("error")
+    }
+
+    console.log(this.regForm.value)
+  }
+
+  pass():ValidatorFn {
+    // @ts-ignore
+    return (formGroup: FormGroup) => {
+      const pass = formGroup.get("password")?.value;
+      const reqPass = formGroup.get("passConfirm")?.value;
+
+      if (reqPass.error && !reqPass.errors.confirmedValidator) {
+        return
+      }
+      if (pass !== reqPass) {
+        return { notSame: true }
+      } else {
+        return null
+      }
+    }
+  }
+
 }
